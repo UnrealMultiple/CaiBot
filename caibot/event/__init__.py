32f894c35265c51959fbdc81e77d5d5f6f9afe7f
ACaiCat <13110818005@qq.com>
2026-04-09 22:00:34 +0800

    fix: 修复事件导入

--- caibot/event/__init__.py
+++ caibot/event/__init__.py
@@ -1,3 +1,10 @@
-from group_join import group_join
-from invite_request import friend_request, group_request
-from join_request import join_request
+from .group_join import group_join
+from .invite_request import friend_request, group_request
+from .join_request import join_request
+
+__all__ = [
+    "group_join",
+    "friend_request",
+    "group_request",
+    "join_request",
+]
